fa462fa8bd1d333c107c5ca91bcbafc7b16f43c4
gamer-12748 <aayushgarg2006@gmail.com>
2021-09-15 17:22:24 +0530

    hmm

--- reusable/misc_reuse.js
+++ reusable/misc_reuse.js
@@ -1,7 +1,28 @@
+const https = require('https')
+
 function validateEmail(email) {
     const re =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-module.exports={validateEmail}
+async function fetch_get(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (resp) => {
+            let data = ''
+
+            resp.on('data', (chunk) => {
+                data += chunk
+            });
+
+            resp.on('end', () => {
+                resolve(JSON.parse(data))
+            });
+
+        }).on('error', (err) => {
+            reject('Error: ' + err.message);
+        })
+    })
+}
+
+module.exports = { validateEmail }
