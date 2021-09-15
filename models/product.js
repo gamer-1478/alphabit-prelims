6851d6046596dfff55521fecf0ce7c132209c021
gamer-12748 <aayushgarg2006@gmail.com>
2021-09-15 17:15:50 +0530

    hmm

--- models/product.js
+++ models/product.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-const reqString = {type:String, required:true}
-const reqNumber = {type:Number, required: true}
+const reqString = { type: String, required: true }
+const reqNumber = { type: Number, required: true }
+
 const productSchema = new Schema({
     "title": reqString,
     "description": reqString,
     "price": reqNumber,
-
+    "image_": reqString,
+    'category': reqString,
+    'rating': {
+        'rate': reqNumber,
+        'count': reqNumber
+    }
 })
+
 const Product = mongoose.model('Product', productSchema)
 module.exports = Product;
--- routes/consumerRoute.js
+++ routes/consumerRoute.js
@@ -2,23 +2,28 @@ const express = require('express')
 const router = express.Router();
 const { checkAuthenticated, checkNotAuthenticated } = require('../reusable/passport_reuse')
 const Product = require('../models/product')
-const https = require('https')
+const { fetch_get } = require('../reusable/misc_reuse')
 const products = {}
-router.get('/', (req, res) => {
-    Product.find().then((result)=> {
-        if(result.length) {
-            console.log(result)
-        } else {
-            https.get('https://fakestoreapi.com/products/1', (resp)=> {
-                resp.on('data', (data)=> {
-                    const products = JSON.parse(data);
-                })
-            })
 
+router.get('/', (req, res) => {
+    Product.find().then((result) => {
+        if (result.length) {
+            console.log(result)
         }
     })
-    res.render('pages/type/consumer', {"title":'Consumer Dashboard', "user":req.user, "products": products})
+    res.render('pages/type/consumer', { "title": 'Consumer Dashboard', "user": req.user, "products": products })
 })
 
+/*router.get('/add_data', async (req, res) => {
+    const response = await fetch_get('https://fakestoreapi.com/products/')
+    await response.forEach(async element => {
+        var prod = new Product({ title: element.title, description: element.description, price: element.price, category:element.category, image_: element.image, rating: { rate: element.rating.rate, count: element.rating.count } })
+        await prod.save(function (err, book) {
+            if (err) return console.error(err);
+            console.log(book.name + " saved to bookstore collection.");
+        });
+    });
+    res.send('hi')
+})*/
 
 module.exports = router;
