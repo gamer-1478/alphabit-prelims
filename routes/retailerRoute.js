8b4acd1ea450d5ab296a3e56c2dd740640e69cc1
gamer-12748 <aayushgarg2006@gmail.com>
2021-09-15 21:13:43 +0530

    done add products

--- routes/retailerRoute.js
+++ routes/retailerRoute.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const router = express.Router();
 const { checkAuthenticated, checkNotAuthenticated } = require('../reusable/passport_reuse')
+const Product = require('../models/product')
+
+
+router.post('/sellForm', async (req, res) => {
+    console.log(req.body)
+    var prod = new Product({ title: req.body.title, description: req.body.description, price: req.body.price, category: req.body.category, image_: req.body.image, rating: { rate: 5, count: 0 } })
+    await prod.save(function (err, product) {
+        if (err) { 
+            res.send({ message: 'Product Failed To Be Added', sucess: false, error:err })
+            return console.error(err) 
+        };
+        res.send({ message: 'Product Added Successfully', sucess: true })
+        console.log(product.title + " saved to products collection.");
+    });
+})
 
 module.exports = router;
